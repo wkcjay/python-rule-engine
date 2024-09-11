4429c8eb8f9ed9af0783cdc1fff3813b30cdcf29
Wong Khia Chen <111277451+wkcjay@users.noreply.github.com>
2024-09-11 10:49:49 +0800

    test pre-commit hook (#3)

--- api/routes/hello_world_router.py
+++ api/routes/hello_world_router.py
@@ -3,10 +3,16 @@ from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
-@router.get("/hello-world", tags=["Hello World"], status_code=status.HTTP_200_OK, description="Hello World")
+
+@router.post(
+    "/hello-world",
+    tags=["Hello World"],
+    status_code=status.HTTP_200_OK,
+    description="Hello World",
+)
 async def hello_world(request: Request, response: Response):
     try:
         return "hello world"
     except Exception as e:
         response.status_code = status.HTTP_400_BAD_REQUEST
-        return jsonable_encoder({'Exception': str(e)})
+        return jsonable_encoder({"Exception": str(e)})
